2ec1e8f91635febfaae32be16ebd24dbaa46bb74
SeanTshark <donotreply@blank.com>
2026-06-17 16:59:16 +0800

    feat: Inital class structure

---
+++ app/utils/exception_class.py
@@ -0,0 +1,120 @@
+class HttpExceptionError(Exception):
+    def __init__(self, status_code):
+        self.status_code = object
+
+
+def http_exception_error(status_code):
+    if status_code == 100:
+        pass
+    if status_code == 101:
+        pass
+    if status_code == 102:
+        pass
+    if status_code == 103:
+        pass
+    if status_code == 300:
+        pass
+    if status_code == 301:
+        pass
+    if status_code == 302:
+        pass
+    if status_code == 303:
+        pass
+    if status_code == 304:
+        pass
+    if status_code == 307:
+        pass
+    if status_code == 308:
+        pass
+    if status_code == 400:
+        pass
+    if status_code == 401:
+        pass
+    if status_code == 402:
+        pass
+    if status_code == 403:
+        pass
+    if status_code == 404:
+        pass
+    if status_code == 405:
+        pass
+    if status_code == 406:
+        pass
+    if status_code == 407:
+        pass
+    if status_code == 408:
+        pass
+    if status_code == 409:
+        pass
+    if status_code == 410:
+        pass
+    if status_code == 411:
+        pass
+    if status_code == 412:
+        pass
+    if status_code == 413:
+        pass
+    if status_code == 414:
+        pass
+    if status_code == 415:
+        pass
+    if status_code == 416:
+        pass
+    if status_code == 417:
+        pass
+    if status_code == 418:
+        pass
+    if status_code == 421:
+        pass
+    if status_code == 422:
+        pass
+    if status_code == 423:
+        pass
+    if status_code == 424:
+        pass
+    if status_code == 425:
+        pass
+    if status_code == 426:
+        pass
+    if status_code == 428:
+        pass
+    if status_code == 429:
+        pass
+    if status_code == 431:
+        pass
+    if status_code == 451:
+        pass
+    if status_code == 500:
+        pass
+    if status_code == 501:
+        pass
+    if status_code == 502:
+        pass
+    if status_code == 503:
+        pass
+    if status_code == 504:
+        pass
+    if status_code == 505:
+        pass
+    if status_code == 506:
+        pass
+    if status_code == 507:
+        pass
+    if status_code == 508:
+        pass
+    if status_code == 510:
+        pass
+    if status_code == 511:
+        pass
+    if status_code == -32700:
+        pass
+    if status_code == -32600:
+        pass
+    if status_code == -32601:
+        pass
+    if status_code == -32602:
+        pass
+    if status_code == -32603:
+        pass
+    if status_code == -32000 or -32099:
+
